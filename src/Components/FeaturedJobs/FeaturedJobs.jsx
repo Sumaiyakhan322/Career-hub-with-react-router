f5c0a007316f5f82364dfdcd730826e79d447bb3
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2023-09-23 20:41:56 +0600

    added all jobs and show more btn

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,6 +3,7 @@ import Job from '../Job/Job';
 
 const FeaturedJobs = () => {
     const [jobs,setjobs]=useState([]);
+    const[datalength,setdataLength]=useState(4);
     useEffect(()=>{
         fetch('../jobs.json')
         .then(res=>res.json())
@@ -17,8 +18,16 @@ const FeaturedJobs = () => {
         </div>
       <div className='grid grid-cols-2 gap-10 mt-16'>
       {
-            jobs.map(job=><Job key={job.id} job={job}></Job>)
+            jobs.slice(0,datalength).map(job=><Job key={job.id} job={job}></Job>)
         }
+      </div>
+      <div>
+        <div className={
+            datalength===jobs.length && 'hidden'}>
+                 <button onClick={()=>setdataLength(6)} className='bg-gradient-to-r from-indigo-300 to-pink-400 px-6 py-3 rounded-lg my-10 block mx-auto' 
+         >Show all</button>
+            </div>
+       
       </div>
         </>
     );
